7a3894d82b50d29abd0a8b1649d193bf16f090c5
GullentopsC <cedricgullentops@hotmail.com>
2020-10-16 20:16:56 +0200

    First Commit Base Site

---
+++ WebPage/BullseyeWebPage/Data/BullseyeWebPageContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BullseyeWebPage.Models;
+
+namespace BullseyeWebPage.Data
+{
+    public class BullseyeWebPageContext : DbContext
+    {
+        public BullseyeWebPageContext (DbContextOptions<BullseyeWebPageContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BullseyeWebPage.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ WebPage/BullseyeWebPage/Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using BullseyeWebPage.Data;
+using BullseyeWebPage.Models;
+
+namespace BullseyeWebPage.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly BullseyeWebPage.Data.BullseyeWebPageContext _context;
+
+        public IndexModel(BullseyeWebPage.Data.BullseyeWebPageContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Movie = await _context.Movie.ToListAsync();
+        }
+    }
+}
